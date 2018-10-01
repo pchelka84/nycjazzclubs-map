d214495587e1b7ed01de57c7f9e23fc2cff25d83
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-10-01 16:02:15 -0400

    Array of objects/locations received

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
+import axios from "axios";
+
 class App extends Component {
+  state = {
+    venues: []
+  };
+
   componentDidMount() {
+    this.getVenues();
     this.renderMap();
   }
 
   initMap = () => {
     var map = new window.google.maps.Map(document.getElementById("map"), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8
+      center: { lat: 40.7127753, lng: -74.0059728 },
+      zoom: 12
     });
+  };
+
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?";
+    const parameters = {
+      client_id: "VMTRPTW1INEDWY1XBEBCGXQMWKT15JJOI22XOX1NX15HOKWV",
+      client_secret: "FWESS4RSP43RA1SARGVZFPC0SJRYEG4QSUYKS345CFFF5MFY",
+      query: "coffee",
+      near: "New York",
+      v: "20181001"
+    };
+
+    axios
+      .get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        this.setState(
+          {
+            venues: response.data.response.groups[0].items
+          },
+          this.renderMap()
+        );
+      })
+      .catch(error => {
+        console.log("ERROR!! " + error);
+      });
   };
 
   renderMap = () => {
