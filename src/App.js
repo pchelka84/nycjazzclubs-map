64f3a9c5f9e6ee4582bb6d4eaeec61304a58d832
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-10-04 16:23:33 -0400

    Layout components are created. SideBar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import SideBar from "./components/SideBar";
+
 import axios from "axios";
 
 class App extends Component {
@@ -99,45 +101,7 @@ class App extends Component {
           </div>
         </nav>
         <main>
-          <div className="sidebar p-3 bg-light m-0">
-            <span
-              className="sidebar-close pt-3 float-right"
-              onClick={() => {
-                const sidebar = document.querySelector(".sidebar");
-                sidebar.classList.add("close");
-              }}
-            >
-              <i className="fas fa-times-circle" />
-            </span>
-
-            <div className="sidebar-header pt-3">
-              <h3>Venues</h3>
-            </div>
-
-            <div class="input-group input-group-sm mt-3 mb-3">
-              <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroup-sizing-sm">
-                  Filter
-                </span>
-              </div>
-              <input
-                type="text"
-                class="form-control"
-                // aria-label="Filter"
-                // aria-describedby="Filter"
-              />
-            </div>
-
-            <nav id="sidebar">
-              <ul className="list-unstyled">
-                <li>Jazz Club 1</li>
-                <li>Jazz Club 2</li>
-                <li>Jazz Club 3</li>
-                <li>Jazz Club 4</li>
-                <li>Jazz Club 5</li>
-              </ul>
-            </nav>
-          </div>
+          <SideBar />
           <div id="map" />
         </main>
       </div>
---
+++ src/components/SideBar.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import "./SideBar.css";
+
+class SideBar extends Component {
+  render() {
+    return (
+      <div className="sidebar p-3 bg-light m-0">
+        <span
+          className="sidebar-close pt-3 float-right"
+          onClick={() => {
+            const sidebar = document.querySelector(".sidebar");
+            sidebar.classList.add("close");
+          }}
+        >
+          <i className="fas fa-times-circle" />
+        </span>
+
+        <div className="sidebar-header pt-3">
+          <h3>Venues</h3>
+        </div>
+
+        <div class="input-group input-group-sm mt-3 mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-sm">
+              Filter
+            </span>
+          </div>
+          <input
+            type="text"
+            class="form-control"
+            // aria-label="Filter"
+            // aria-describedby="Filter"
+          />
+        </div>
+
+        <nav id="sidebar">
+          <ul className="list-unstyled">
+            <li>Jazz Club 1</li>
+            <li>Jazz Club 2</li>
+            <li>Jazz Club 3</li>
+            <li>Jazz Club 4</li>
+            <li>Jazz Club 5</li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default SideBar;
